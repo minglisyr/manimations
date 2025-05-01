0c0289b91d746b43b23a9abda6b6eecf2d43c727
minglisyr <ml2954@DEN19Q3HM2.na.corp>
2025-05-01 16:31:38 -0600

    import adaption

--- manim_imports_ext.py
+++ manim_imports_ext.py
@@ -1,5 +1,5 @@
-from manimlib import *
-from manimlib.mobject.svg.old_tex_mobject import *
+from manim import *
+from manim.mobject.svg.svg_mobject import *
 
 from custom.backdrops import *
 from custom.banner import *
